6261f29f6ed0e837592fbb1a30e8424a11855136
Nadav Mermer <mermer@bigpanda.io>
2020-03-27 18:02:11 +0300

    Add script for downloading corona latest data

---
+++ download_data.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+mkdir -p datasets/
+curl https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv > datasets/confirmed_$(date +%d-%m).csv
+curl https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv > datasets/deaths_$(date +%d-%m).csv
+curl https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv > datasets/recovered_$(date +%d-%m).csv
